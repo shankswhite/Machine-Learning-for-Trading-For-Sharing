0b9a06df0d8b8cbc2a0fd3798d2a382fc869420e
Xiaofeng Zhao <zxf6911518@qq.com>
2024-09-25 09:37:56 +0800

    Update marketsimcode.py

--- marketsimcode.py
+++ marketsimcode.py
@@ -1,30 +1,4 @@
-""""""  		  	   		 	   			  		 			     			  	 
-"""MC2-P1: Market simulator.  		  	   		 	   			  		 			     			  	 
-  		  	   		 	   			  		 			     			  	 
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		 	   			  		 			     			  	 
-Atlanta, Georgia 30332  		  	   		 	   			  		 			     			  	 
-All Rights Reserved  		  	   		 	   			  		 			     			  	 
-  		  	   		 	   			  		 			     			  	 
-Template code for CS 4646/7646  		  	   		 	   			  		 			     			  	 
-  		  	   		 	   			  		 			     			  	 
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		 	   			  		 			     			  	 
-works, including solutions to the projects assigned in this course. Students  		  	   		 	   			  		 			     			  	 
-and other users of this template code are advised not to share it with others  		  	   		 	   			  		 			     			  	 
-or to make it available on publicly viewable websites including repositories  		  	   		 	   			  		 			     			  	 
-such as github and gitlab.  This copyright statement should not be removed  		  	   		 	   			  		 			     			  	 
-or edited.  		  	   		 	   			  		 			     			  	 
-  		  	   		 	   			  		 			     			  	 
-We do grant permission to share solutions privately with non-students such  		  	   		 	   			  		 			     			  	 
-as potential employers. However, sharing with other current or future  		  	   		 	   			  		 			     			  	 
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		 	   			  		 			     			  	 
-GT honor code violation.  		  	   		 	   			  		 			     			  	 
-  		  	   		 	   			  		 			     			  	 
------do not edit anything above this line---  		  	   		 	   			  		 			     			  	 
-  		  	   		 	   			  		 			     			  	 
-Student Name: Xiaofeng Zhao 		  	   		 	   			  		 			     			  	 
-GT User ID: xzhao474 		  	   		 	   			  		 			     			  	 
-GT ID: 903957020	  	   		 	   			  		 			     			  	 
-"""  		  	   		 	   			  		 			     			  	 
+		 	   			  		 			     			  	 
   		  	   		 	   			  		 			     			  	 
 import datetime as dt  		  	   		 	   			  		 			     			  	 
 import os  		  	   		 	   			  		 			     			  	 
@@ -41,7 +15,7 @@ def author():
     :return: The GT username of the student
     :rtype: str
     """
-    return "xzhao474"
+    return "xz"
 
 
 def compute_portvals(
